ee975d4fb3074b6ff286fcecaa8285d82279d237
Ken Burgess <kenburgess2002@gmail.com>
2023-04-28 23:09:36 +0100

    Attempted implementation of parallax

--- js/scrollTest.js
+++ js/scrollTest.js
@@ -1,18 +1,7 @@
-document.body.onkeyup = function(event) {
-    if (event.keyCode == 49 || event.key == '1') { // Press '1' to scroll right
-        window.scrollBy({
-            top: 0,
-            left: 100,
-            behavior: "smooth",
-          });
-    }
-}
-
 const swiper = new Swiper('.swiper', {
     speed: 600,
+    parallax: true,
     grabCursor: true, // Shows a 'grab' cursor for non-touchscreen devices.
-    resistance: true,  // Resistance to spam?
-    resistanceRatio: 50000, // Default: 0.85 - Set higher to lessen spam-swiping
     direction: 'horizontal',
     loop: false,
   
@@ -31,4 +20,14 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
-  });
+});
+
+document.body.onkeyup = function(event) {
+    if (event.keyCode == 49 || event.key == '1') { // Press '1' to scroll right
+        window.scrollBy({
+            top: 0,
+            left: 100,
+            behavior: "smooth",
+          });
+    }
+}
