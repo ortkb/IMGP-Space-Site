af428ca26fd5b4eaf170a01f40c429721e9b2935
Ken Burgess <kenburgess2002@gmail.com>
2023-04-28 19:50:21 +0100

    removed npm, first swiper added

--- js/scrollTest.js
+++ js/scrollTest.js
@@ -7,3 +7,28 @@ document.body.onkeyup = function(event) {
           });
     }
 }
+
+const swiper = new Swiper('.swiper', {
+    speed: 600,
+    grabCursor: true, // Shows a 'grab' cursor for non-touchscreen devices.
+    resistance: true,  // Resistance to spam?
+    resistanceRatio: 50000, // Default: 0.85 - Set higher to lessen spam-swiping
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
