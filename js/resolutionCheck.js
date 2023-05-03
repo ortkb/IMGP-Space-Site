0e616f03d7a00a3d24c87e3d3611baefd4c42ea4
Ken Burgess <kenburgess2002@gmail.com>
2023-05-04 00:20:31 +0100

    updated error message spread

--- js/resolutionCheck.js
+++ js/resolutionCheck.js
@@ -5,7 +5,8 @@ let errorMessageButton = document.getElementById("errormessage-button");
 // Is screen width is a higher value then screen height?
 let isLandscape = screen.width > screen.height;
 
-//Sourced from "detect-device-type.md" from https://github.com/30-seconds/30-seconds-of-code 
+// Sourced from "detect-device-type.md" from https://github.com/30-seconds/30-seconds-of-code
+// Checks whether the current device matches any of the specified mobile / tablet devices
 const detectDeviceType = () =>
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     ? 'Mobile'
@@ -14,7 +15,8 @@ console.log(detectDeviceType()); // "Mobile" or "Desktop"
 
 if (detectDeviceType() == "Desktop"){
     // if the device is not a mobile device..
-    errorMessageDescription.innerText = "This site is designed for mobile and tablet devices. Please use the side buttons or click + drag to navigate the page.";
+    errorMessageTitle.innerText = "It's better on a tablet / mobile!"
+    errorMessageDescription.innerText = "This site is deigned for mobile and tablet devices. Please use the side buttons or click + drag to navigate the page.";
     // errorMessageButton = should be visible
     // user should be able to close this window if needed.
 }
