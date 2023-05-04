763bd5b1952b1e70e1cde92d9d14730216317882
Ken Burgess <kenburgess2002@gmail.com>
2023-05-04 17:17:42 +0100

    renamed scrollTest.js to main.js, added scrollbar

--- js/main.js
+++ js/main.js
@@ -5,15 +5,16 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: false,
 
+    // Progress bar
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
     },
 });
 
