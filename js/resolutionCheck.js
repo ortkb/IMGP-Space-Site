8b5922ed82b6b08a8bb6033fcebc6ef444a59c1e
Ken Burgess <kenburgess2002@gmail.com>
2023-05-04 15:02:17 +0100

    added writeErrorMessage() function

--- js/resolutionCheck.js
+++ js/resolutionCheck.js
@@ -2,28 +2,55 @@ let errorMessageTitle = document.getElementById("errormessage-title");
 let errorMessageDescription = document.getElementById("errormessage-desc");
 let errorMessageButton = document.getElementById("errormessage-button");
 
-// Is screen width is a higher value then screen height?
-let isLandscape = screen.width > screen.height;
 
-// Sourced from "detect-device-type.md" from https://github.com/30-seconds/30-seconds-of-code
-// Checks whether the current device matches any of the specified mobile / tablet devices
-const detectDeviceType = () =>
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-    ? 'Mobile'
-    : 'Desktop';
-console.log(detectDeviceType()); // "Mobile" or "Desktop"
+let isLandscape = screen.width > screen.height; // Is screen width a higher value then screen height?
 
 if (detectDeviceType() == "Desktop"){
     // if the device is not a mobile device..
+    writeErrorMessage("It's better on a tablet / mobile!", "This site is deigned for mobile and tablet devices. Please use the side buttons or click + drag to navigate the page.");
+    
+    /*
     errorMessageTitle.innerText = "It's better on a tablet / mobile!"
     errorMessageDescription.innerText = "This site is deigned for mobile and tablet devices. Please use the side buttons or click + drag to navigate the page.";
-    // errorMessageButton = should be visible
+    errorMessageButton.style.display = "block";
+    */
     // user should be able to close this window if needed.
 }
 
-if (!isLandscape){
-    // is device is a mobile device but is not landscape..
-    errorMessageDescription.innerText = "Please change to horizontal orientation.";
-    // errorMessageButton = not visible
-    // users SHOULD NOT be able to close this window until horizontal
+function checkLandscapeOrientation(){
+    if (!isLandscape){  // is device is a mobile device but is not landscape..
+        writeErrorMessage("", "Please change to horizontal orientation", false); // users SHOULD NOT be able to close this window until horizontal
+        /*
+        errorMessageDescription.innerText = "Please change to horizontal orientation.";
+        errorMessageButton.style.display = "none";  
+        */
+    }else{
+        // if the device is at landscape 
+        errorMessageButton.style.display = "inline";
+    }
 }
+
+// Sourced from "detect-device-type.md" from https://github.com/30-seconds/30-seconds-of-code
+function detectDeviceType(){
+    // Checks whether the current device matches any of the specified mobile / tablet devices
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? 'Mobile' : 'Desktop';
+}
+
+if (screen.orientation){
+    screen.orientation.addEventListener("change", function(){
+        checkLandscapeOrientation();
+    });
+} else{
+    console.log("screen orientation API is not supported in this browser, sorry!");
+}
+
+function writeErrorMessage(titleText, descText, buttonDisplayBool = true){
+    if(titleText !== "") {errorMessageTitle.innerText = title;}
+    if(descText !== "") {errorMessageDescription.innerText = title;}
+    buttonDisplayBool ? errorMessageButton.style.display = "inline" : errorMessageButton.style.display = "none";
+}
+
+
+window.addEventListener("orientationchange", function() {
+    alert(window.orientation);
+  }, false);
