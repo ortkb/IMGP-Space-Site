5b421c048b9c468e9b680deff834715e4c5bd21b
Ken Burgess <kenburgess2002@gmail.com>
2023-05-07 14:20:42 +0100

    implemented SwipeDirectionDetector

--- js/main.js
+++ js/main.js
@@ -1,44 +1,70 @@
+// Removes loading screen once page has loaded.
 addEventListener("load", function(){ // on page load..
   document.getElementById("loader").style.display = "none";
 });
 
+
+
+
+// ---- Spaceship ----
+
+
+
+
+// ---- Swiper ----
+
+// Initialises the Swiper
 const swiper = new Swiper('.swiper', {
-    speed: 600,
-    parallax: true,
-    /* grabCursor: true, // Shows a 'grab' cursor for non-touchscreen devices. */
-    direction: 'horizontal',
-    loop: false,
+  speed: 600,
+  parallax: true,
+  /* grabCursor: true, // Shows a 'grab' cursor for non-touchscreen devices. */
+  direction: 'horizontal',
+  loop: false,
 
-    // Progress bar
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-    },
+  // Progress bar
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
-document.body.onkeyup = function(event) {
-    if (event.keyCode == 49 || event.key == '1') { // Press '1' to scroll right
-        window.scrollBy({
-            top: 0,
-            left: 100,
-            behavior: "smooth",
-          });
-    }
-}
-
 // check when a swipe has been made
-swiper.on( 'slideChange', function() {
-  if ( swiper.activeIndex > swiper.previousIndex ) {
-      console.log( 'left' );
+swiper.on('slideChange', function() {
+
+  console.log(swiper.activeIndex);
+
+  if (swiper.activeIndex > swiper.previousIndex) {
+      console.log('left');
+      // If swipe
   } else {
       console.log('right');
   }
 });
 
-// get on touch event to switch between spaceship and spaceship flying images 
+// While the user is swiping, finds whether the user is swiping left or right
+swiper.on("touchMove", function(){
+  console.log(swipeDirectionDetector.getSwipeDirection(swiper.touches.diff));
+  // get on touch event to switch between spaceship and spaceship flying images 
+})
+
+// If scrolling to the left, returns '-1', if scrolling right (or not scrolling), returns '1'
+class SwipeDirectionDetector { 
+  prevSwipeDiff = 0;
+
+  getSwipeDirection(newSwipeDiff){
+    if(newSwipeDiff == undefined){
+      console.error("'newSwipeDiff' parameter is not defined.");
+    }
+    let isScrollingLeft = this.prevSwipeDiff < newSwipeDiff;
+    this.prevSwipeDiff = newSwipeDiff;
+    return isScrollingLeft ? -1 : 1; 
+  }
+}
+
+const swipeDirectionDetector = new SwipeDirectionDetector();
